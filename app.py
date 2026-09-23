44fe3ea4d8b234329558f380cb50ba9d74eb94b7
mohan.mk <kakarapathikrishna@gmail.com>
2026-09-23 13:42:11 +0530

    first implemenation to call qwen

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 import requests
 
-OLAMMA_URL = ""
+OLLAMA_URL = "http://localhost:11434/api/generate"
+
+data = {
+    "model": "qwen2.5:3b",
+    "prompt": "Explain Node.js in simple terms",
+    "stream": False
+}
+
+response = requests.post(OLLAMA_URL, json=data)
+result = response.json()
+
+
+print(result['response'])
